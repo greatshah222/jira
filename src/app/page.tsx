12aedd31efec9b184ad84f445abb4e6b3f616ab0
Bishal Shah <great.shah222@gmail.com>
2025-01-05 20:12:57 +0200

    customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<Button variant={"destructive"}>Click me</Button>
+		<div className="flex gap-4">
+			<Button>Click me</Button>
+			<Input />
 		</div>
 	);
 }
